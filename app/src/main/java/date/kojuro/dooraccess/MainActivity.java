75c689bb3a3c330ba86f330fc68c6fa416e41e3e
tjjh89017 <tjjh89017@hotmail.com>
2017-02-14 20:29:58 +0800

    test

--- app/src/main/java/date/kojuro/dooraccess/MainActivity.java
+++ app/src/main/java/date/kojuro/dooraccess/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.FragmentManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -132,6 +133,14 @@ public class MainActivity extends AppCompatActivity {
         vNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId()) {
+                    case R.id.nav_menu_home:
+                        break;
+                    case R.id.nav_menu_location:
+                        break;
+                    default:
+                }
 
                 Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
 
